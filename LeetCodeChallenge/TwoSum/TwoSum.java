4c5b592c433703ec5c240864755abe8ea0e7026f
tls <thantlwinsoe385@gmail.com>
2022-09-15 23:46:51 +0630

    Update TwoSum

--- LeetCodeChallenge/TwoSum/TwoSum.java
+++ LeetCodeChallenge/TwoSum/TwoSum.java
@@ -8,8 +8,22 @@ public class TwoSum {
 		// To Sum Two Numbers 
 		for(int i = 0 ; i < num.length-1 ; i ++) {
 
+			for(int j = i + 1 ; j < num.length ; j++) {
+
+							// match target
+				if( target == (num[i] + num[j]) ){
+					// One index increase From Original Array and assign Original index value
+					result = Arrays.copyOf(result , result.length + 1);
+					result[result.length - 1] = i;
+					// then one more index increase From Original Array and assign Original index value
+					result = Arrays.copyOf(result , result.length + 1);
+					result[result.length - 1] = j;
+
+				}
+			}
+
 			// match target
-			if( target == (num[i] + num[i+1]) ){
+/*			if( target == (num[i] + num[i+1]) ){
 				// One index increase From Original Array and assign Original index value
 				result = Arrays.copyOf(result , result.length + 1);
 				result[result.length - 1] = i;
@@ -18,7 +32,7 @@ public class TwoSum {
 				result[result.length - 1] = i+1;
 
 			}
-
+*/
 		}
 
 		return result;
