187c1606795ac01e65b2331555bb2dd22274e71b
tls <thantlwinsoe385@gmail.com>
2022-09-20 00:22:07 +0630

    Edit BAG

--- EdabitChallenge/BasicArithmeticGivenString/BasicArithmeticGivenString.java
+++ EdabitChallenge/BasicArithmeticGivenString/BasicArithmeticGivenString.java
@@ -1,20 +1,61 @@
+/* Problem From Edabit.com 
+
+Write a function that does the following for the given values: add, subtract, divide and multiply. These are simply referred to as the basic arithmetic operations. The variables have to be defined, but in this challenge they will be defined for you. All you have to do is check the variables, do some string to integer conversions, use some if conditions, and apply the arithmetic operation.
+Examples
+
+operation("1",  "2",  "add" ) ➞ 3
+
+operation("4",  "5",  "subtract") ➞ -1
+
+operation("6",  "3",  "divide") ➞ 2
+
+Notes
+
+    The numbers and operation are given as strings and should result in an integer value.
+    If the operation results in an ArithmeticException, then return Integer.MIN_VALUE (e.g. division by zero).
+    Division results will be rounded down to its integral part.
+
+*/
 public class BasicArithmeticGivenString {
 
 	public static int operation(String op1, String op2, String operator) {
-		int result = 0;
 
 		switch (operator) {
 			case "sum":
-				result = sum(op1,op2);
-				break;
+				return sum(op1,op2);
+			//	break;
+			case "subtract":
+				return subtract(op1,op2);
+			case "multiply": 
+				return multiply(op1,op2);
+			case "divide":
+				
+				try{
+					return divide(op1,op2);
+				} catch(ArithmeticException e) {
+					System.out.println("Operand 2 is Zero!!");
+					return Integer.MIN_VALUE;
+				}
 			default:
 				System.out.println("Enter Valid Operator");
-				break;
+				return 0;
 
 		}
 
-		return result;
+	}
 
+	public static int divide(String op1, String op2) {
+		return Integer.valueOf(op1) / Integer.valueOf(op2);
+	}
+
+	public static int multiply(String op1, String op2) {
+
+		return Integer.valueOf(op1) * Integer.valueOf(op2);
+	}
+
+	public static int subtract(String op1, String op2) {
+
+		return Integer.valueOf(op1) - Integer.valueOf(op2);
 	}
 
 	public static int sum(String op1, String op2) {
@@ -25,4 +66,5 @@ public class BasicArithmeticGivenString {
 		return i+j;
 
 	}
+
 }
