747a682029e8dc02ea0b7906449714159c9607f9
tls <thantlwinsoe385@gmail.com>
2022-10-26 00:31:57 +0630

    Edit First Servlet

---
+++ Servlet/01-first-Servlet/src/main/java/com/tls/ServletDemo/ForFirstServlet.java
@@ -0,0 +1,34 @@
+package com.tls.ServletDemo;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class ForFirstServlet extends HttpServlet{
+
+	private static final long serialVersionUID = 1L;
+	
+	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		int i = Integer.parseInt(req.getParameter("num1"));
+		int j = Integer.parseInt(req.getParameter("num2"));
+		
+		int k = i +j;
+		//System.out.println("Result is :" + k);
+		
+		res.getWriter().println("Result is :" + k);
+	}
+	
+	
+/*	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		int i = Integer.parseInt(req.getParameter("num1"));
+		int j = Integer.parseInt(req.getParameter("num2"));
+		
+		int k = i +j;
+		System.out.println("Result is :" + k);
+	}
+*/
+}
