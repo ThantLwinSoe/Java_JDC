47f84a93aae3a41546ee973d617abf7d10a5eda2
tls <thantlwinsoe385@gmail.com>
2023-01-21 23:54:16 +0630

    Edit Intro LongestPalindormeSubstring

---
+++ LeetCodeChallenge/LongestPalindromicSubstring/LongestPalindormic.java
@@ -0,0 +1,24 @@
+class LongestPalindormic {
+
+	public static String getLongestPalindrome(String str) {
+
+		int mid = str.length() / 2 ;
+		int mid_rigth = 0;
+		int mid_left = 0;
+
+		if(mid == 0) {
+			return str;
+		}
+
+		do {
+
+			mid_right = mid + 1;
+			mid_left = mid - 1;
+
+		}while();
+
+
+
+		return null;
+	}
+}
